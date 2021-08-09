244aa4011e388c31c806630aab895cc0a92f32e6
siege <70180119+n0tsiege@users.noreply.github.com>
2021-08-09 10:04:27 -0400

    update: __resource to fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DevLanceGood + n0tsiege'
+description 'A configurable greenzone/safezone script for your FiveM server!'
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+}
